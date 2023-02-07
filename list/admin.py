a7fd9090b38f7a31303aa42ef1c0354ca49f9ac6
Diana <daria.blessed18@gmail.com>
2023-02-07 12:33:53 +0200

    Create admin panel

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import Group
+
+from .models import Tag, Task
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Task)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("content", )
+    list_filter = ("done",)
+
+
+admin.site.register(Tag)
+admin.site.unregister(Group)
