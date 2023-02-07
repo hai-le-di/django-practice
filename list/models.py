c2c5476e89f45f7c02664e30c951b65f6a997183
Diana <daria.blessed18@gmail.com>
2023-02-07 12:15:36 +0200

    Create Task & Tag models

--- list/models.py
+++ list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateField(auto_now_add=True)
+    deadline = models.DateField(auto_now=True, blank=True)
+    done = models.BooleanField()
+    not_relevant_tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks"
+    )
+
+
+class Tag(models.Model):
+    tasks = models.ManyToManyField(Task, related_name="tags")
