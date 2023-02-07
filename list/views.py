ea664bdd39bc707b6debb9ad63b567dfc2c06c95
Diana <daria.blessed18@gmail.com>
2023-02-07 12:49:49 +0200

    Create index view

--- list/views.py
+++ list/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from models import Tag, Task
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_tags = Tag.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tags": num_tags,
+        "num_tasks": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "list/index.html", context=context)
