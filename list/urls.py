014ee002620f020df1b18cd41050bb636618ee99
Diana <daria.blessed18@gmail.com>
2023-02-07 14:35:55 +0200

    Create tasks/create url

--- list/urls.py
+++ list/urls.py
@@ -3,7 +3,8 @@ from .views import (
     index,
     TaskListView,
     TaskUpdateView,
-    TaskDeleteView
+    TaskDeleteView,
+    TaskCreateView,
 )
 
 urlpatterns = [
@@ -11,6 +12,7 @@ urlpatterns = [
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 
