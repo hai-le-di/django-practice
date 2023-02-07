e1ee4cadeddd5971bcafff85cd131c0cd4e5e5bf
Diana <daria.blessed18@gmail.com>
2023-02-07 15:16:14 +0200

    Update home page

--- list/urls.py
+++ list/urls.py
@@ -12,8 +12,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
--- list/views.py
+++ list/views.py
@@ -5,24 +5,6 @@ from django.views import generic
 from .models import Tag, Task
 
 
-def index(request):
-    """View function for the home page of the site."""
-
-    num_tags = Tag.objects.count()
-    num_tasks = Task.objects.count()
-
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
-    context = {
-        "num_tags": num_tags,
-        "num_tasks": num_tasks,
-        "num_visits": num_visits + 1,
-    }
-
-    return render(request, "list/index.html", context=context)
-
-
 class TaskListView(generic.ListView):
     model = Task
 
