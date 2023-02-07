23df30f4bc1b7f3ac3503d54aac69c2590881873
Diana <daria.blessed18@gmail.com>
2023-02-07 15:05:34 +0200

    Add tag create & delete & update urls

--- list/urls.py
+++ list/urls.py
@@ -6,6 +6,9 @@ from .views import (
     TaskDeleteView,
     TaskCreateView,
     TagListView,
+    TagUpdateView,
+    TagDeleteView,
+    TagCreateView,
 )
 
 urlpatterns = [
@@ -15,6 +18,9 @@ urlpatterns = [
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 
