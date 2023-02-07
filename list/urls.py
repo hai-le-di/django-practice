b4fe242c91029dcd7dcdfc866f0990f20bc67eed
Diana <daria.blessed18@gmail.com>
2023-02-07 14:24:54 +0200

    Add update & delete urls

--- list/urls.py
+++ list/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import index, TaskListView
+from .views import (
+    index,
+    TaskListView,
+    TaskUpdateView,
+    TaskDeleteView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 
